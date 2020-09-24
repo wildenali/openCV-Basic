8343b12718bd88c1e5211b3556d6a97e54edc0c4
wildenali <wildenwildenaliali@gmail.com>
2020-09-24 23:09:53 +0700

    jika di pencet selain tombol esc, dia akan nge save

--- 04_Pencet_s_untuk_Save/04_pencet_s_untuk_Save.py
+++ 04_Pencet_s_untuk_Save/04_pencet_s_untuk_Save.py
@@ -4,20 +4,23 @@ import cv2
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html
 
 # 1. membuat objek dengan nama img, untuk menampung gambarnya
-img = cv2.imread('test_gambar.png', 1)      # kalau nilainya 1 -> berwarna, kalau 0 jadi hitam putih
+# kalau nilainya 1 -> berwarna, kalau 0 jadi hitam putih
+img = cv2.imread('test_gambar.png', 1)
 
 # 2. cara menampilkan gambar si objek img
 cv2.imshow('gambarnya', img)
 
-# 4. cara menambahkan aksi jika di pencet s maka gambar akan di save
+# 4. cara menambahkan aksi jika di pencet kecuali esc maka gambar akan di save
 k = cv2.waitKey(0)
 if k == 27:
+    print(k)
     cv2.destroyAllWindows()
 else:
     # 3. cara nge write gambar atau nge save/save as gambar
+    print(k)
     cv2.imwrite('gambar_baru.png', img)
-    
+
     # 3. test save gambar menjadi hitam putih
+    print(k)
     img_hitam_putih = cv2.imread('test_gambar.png', 0)
     cv2.imwrite('gambar_hitam_putih.png', img_hitam_putih)
-
