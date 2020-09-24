c039b18ad5b0eecf15002448fa790041f693bf23
wildenali <wildenwildenaliali@gmail.com>
2020-09-24 23:17:04 +0700

    coba warna lain

--- 05_Tampil_Gambar_pake_Matplotilb/05_Tampil_Gambar_pake_Matplotilb.py
+++ 05_Tampil_Gambar_pake_Matplotilb/05_Tampil_Gambar_pake_Matplotilb.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt    # tambahan untuk latihan no 05_Tampil_Gambar_pake_Matplotilb
+# tambahan untuk latihan no 05_Tampil_Gambar_pake_Matplotilb
+from matplotlib import pyplot as plt
 
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html
 
 # 1. membuat objek dengan nama img, untuk menampung gambarnya
-img = cv2.imread('test_gambar.png', 1)      # kalau nilainya 1 -> berwarna, kalau 0 jadi hitam putih
+# kalau nilainya 1 -> berwarna, kalau 0 jadi hitam putih
+img = cv2.imread('test_gambar.png', 1)
 
 # 5. cara menampilkan gambar dengan tambahan Matplotlib
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.imshow(img, cmap='pink', interpolation='bicubic')
 plt.xticks([]), plt.yticks([])     # to hide tick values on X and Y axis
 plt.show()
