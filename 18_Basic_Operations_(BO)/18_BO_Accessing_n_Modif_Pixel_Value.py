e0fa0a80f45ab5ee0df06b99ccfd4c5f13a310b0
wildenali <wildenwildenaliali@gmail.com>
2020-09-24 23:34:13 +0700

    Benerin syntax print

--- 18_Basic_Operations_(BO)/18_BO_Accessing_n_Modif_Pixel_Value.py
+++ 18_Basic_Operations_(BO)/18_BO_Accessing_n_Modif_Pixel_Value.py
@@ -10,30 +10,31 @@ img = cv2.imread('roi.jpg')
 # for BGR image, it returns an array of Blue, Gree, Red values
 # for Grayscale image, just coresponding intensity is returned
 
-px = img[100,100]
-print px
+px = img[100, 100]
+print(px)
 
 # accessing only blue pixel
-blue = img[100,100,0]
-print blue
+blue = img[100, 100, 0]
+print(blue)
 
 # Modif the pixel value
-img[100,100] = [255,255,255]
-print img[100,100]
+img[100, 100] = [255, 255, 255]
+print(img[100, 100])
 
 # Accessing RED value
-img.item(10,10,2)
-print img.item(10,10,2)
+img.item(10, 10, 2)
+print(img.item(10, 10, 2))
 
 # modifying RED value
-img.itemset((10,10,2),100)
-img.item(10,10,2)
-print img.item(10,10,2)
+img.itemset((10, 10, 2), 100)
+img.item(10, 10, 2)
+print(img.item(10, 10, 2))
 
 # Accessing Image Properties
-print img.shape
-print img.size
-print img.dtype     # ini sangat penting, karena kebanyakan kasus error di OpenCV karena salah tipe data
+print(img.shape)
+print(img.size)
+# ini sangat penting, karena kebanyakan kasus error di OpenCV karena salah tipe data
+print(img.dtype)
 
 # Image ROI (Region of Images)
 # beluuum, tutorial di link nya error hasilnya
