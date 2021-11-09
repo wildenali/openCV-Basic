29c65d66f82a5596da6be0c9c98fa87cc7be32eb
Wilden Ali <wildenwildenaliali@gmail.com>
2021-11-09 16:10:23 +0700

    how to create jpg file using opencv

--- 03_Tulis_Gambar (Save_Image)/03_Tulis_Gambar.py
+++ 03_Tulis_Gambar (Save_Image)/03_Tulis_Gambar.py
@@ -12,6 +12,11 @@ cv2.imshow('gambarnya', img)
 # 3. cara nge write gambar atau nge save/save as gambar
 cv2.imwrite('gambar_baru.png', img)
 
+for i in range(0,5):
+    nama_gambar = 'gambar_baru' + '_' + str(i) + '.png'
+    print(nama_gambar)
+    cv2.imwrite(nama_gambar, img)    
+
 # 3. test save gambar menjadi hitam putih
 img_hitam_putih = cv2.imread('test_gambar.png', 0)
 cv2.imwrite('gambar_hitam_putih.png', img_hitam_putih)
