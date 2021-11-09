8ba2c71e1bac7cd0dcd7fbbfe584edf9a8db3e96
Wilden Ali <wildenwildenaliali@gmail.com>
2021-11-09 17:19:27 +0700

    typo

--- 20_Measurement_Techniques/20_Measurement_Techniques.py
+++ 20_Measurement_Techniques/20_Measurement_Techniques.py
@@ -7,8 +7,8 @@ import numpy as np
 img1 = cv2.imread('messi.jpg')
 
 e1 = cv2.getTickCount()
-for i in xrange(5,39,2):
+for i in range(5,39,2):
     img1 = cv2.medianBlur(img1,i)
 e2 = cv2.getTickCount()
 t = (e2 - e1)/cv2.getTickFrequency()
-print t
+print(t)
